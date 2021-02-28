c8a871940082e2033de95c709013f2069a8e6306
Taylor Spencer <tayspen72@gmail.com>
2021-02-27 21:59:55 -0700

    clarity updates, bug fixes

--- src/mcu/spi.rs
+++ src/mcu/spi.rs
@@ -7,7 +7,10 @@
 // Crates and Mods
 //==============================================================================
 use nrf52832_pac::spi0;
+use crate::config;
 use crate::mcu::gpio;
+use nrf52832_pac::p0::pin_cnf::DIR_A as DIR;
+use nrf52832_pac::p0::pin_cnf::PULL_A as PULL;
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -15,7 +18,6 @@ use crate::mcu::gpio;
 #[allow(dead_code)]
 pub struct SpiLine{
 	pub sclk_pin: u8,
-	pub sel_pin: u8,
 	pub mosi_pin: u8,
 	pub miso_pin: u8,
 	pub frequency: spi0::frequency::FREQUENCY_A,
@@ -32,74 +34,86 @@ pub struct SpiLine{
 //==============================================================================
 // Variables
 //==============================================================================
+static SPI_LINE: SpiLine = SpiLine {
+	sclk_pin: config::SPI_SCLK_PIN,
+	mosi_pin: config::SPI_MOSI_PIN,
+	miso_pin: config::SPI_MISO_PIN,
+	frequency: config::SPI_FREQUENCY,
+	order: config::SPI_ORDER,
+	cpha: config::SPI_CPHA,
+	cpol: config::SPI_CPOL,
+};
 
+static mut _INITIALIZED: bool = false;
 
 //==============================================================================
 // Implementations
 //==============================================================================
 #[allow(dead_code)]
-pub fn init(p: &nrf52832_pac::Peripherals, spiline: &SpiLine) {
+pub fn init(p: &nrf52832_pac::Peripherals) {
 	let spi = &p.SPI0;
 
 	spi.enable.write(|w| w.enable().disabled());
 
 	// Configure MOSI pin
-	gpio::pin_setup(p, spiline.mosi_pin, nrf52832_pac::p0::pin_cnf::DIR_A::OUTPUT, nrf52832_pac::p0::pin_cnf::PULL_A::DISABLED, gpio::PinState::PinLow);
-	spi.psel.mosi.write(|w| unsafe { w.bits(spiline.mosi_pin as u32) });
+	gpio::pin_setup(p, SPI_LINE.mosi_pin, DIR::OUTPUT, gpio::PinState::PinLow, PULL::DISABLED);
+	spi.psel.mosi.write(|w| unsafe { w.bits(SPI_LINE.mosi_pin as u32) });
 
 	// Configure MISO pin
-	gpio::pin_setup(p, spiline.miso_pin, nrf52832_pac::p0::pin_cnf::DIR_A::OUTPUT, nrf52832_pac::p0::pin_cnf::PULL_A::DISABLED, gpio::PinState::PinLow);
-	spi.psel.miso.write(|w| unsafe { w.bits(spiline.miso_pin as u32) });
+	gpio::pin_setup(p, SPI_LINE.miso_pin, DIR::INPUT, gpio::PinState::PinHigh, PULL::PULLUP);
+	spi.psel.miso.write(|w| unsafe { w.bits(SPI_LINE.miso_pin as u32) });
 
 	// Configure SCLK pin
-	spi.psel.sck.write(|w| unsafe { w.bits(spiline.sclk_pin as u32) });
+	gpio::pin_setup(p, SPI_LINE.sclk_pin, DIR::OUTPUT, gpio::PinState::PinLow, PULL::DISABLED);
+	spi.psel.sck.write(|w| unsafe { w.bits(SPI_LINE.sclk_pin as u32) });
 
-	// Configure SEL pin
-	gpio::pin_setup(p, spiline.sel_pin, nrf52832_pac::p0::pin_cnf::DIR_A::OUTPUT, nrf52832_pac::p0::pin_cnf::PULL_A::DISABLED, gpio::PinState::PinHigh);
-
-	spi.frequency.write(|w| w.frequency().variant(spiline.frequency));
+	spi.frequency.write(|w| w.frequency().variant(SPI_LINE.frequency));
 	spi.config.write(|w| w
-		.order().variant(spiline.order)
-		.cpha().variant(spiline.cpha)
-		.cpol().variant(spiline.cpol)
+		.order().variant(SPI_LINE.order)
+		.cpha().variant(SPI_LINE.cpha)
+		.cpol().variant(SPI_LINE.cpol)
 	);
+
+	spi.enable.write(|w| w.enable().enabled());
+
+	unsafe { _INITIALIZED = true };
 }
 
 #[allow(dead_code)]
-pub fn tx_byte(p: &nrf52832_pac::Peripherals, spiline: &SpiLine, byte: u8) {
-	set_sel(p, spiline, true);
+pub fn tx_block(p: &nrf52832_pac::Peripherals, block: &[u8]) {
+	if !unsafe { _INITIALIZED }{
+		init(p);
+	}
+
+	p.SPIM0.txd.maxcnt.write(|w| unsafe { w.maxcnt().bits(block.len() as u8) });
+	p.SPIM0.txd.ptr.write(|w| unsafe { w.ptr().bits( block.as_ptr() as u32) });
+}
+
+#[allow(dead_code)]
+pub fn tx_byte(p: &nrf52832_pac::Peripherals, byte: u8) {
+	if !unsafe { _INITIALIZED }{
+		init(p);
+	}
 
 	p.SPI0.txd.write(|w| unsafe { w.txd().bits(byte) });
 
-	while p.SPI0.events_ready.read().bits() != 0 {};
-
-	set_sel(p, spiline, false);
+	while p.SPI0.events_ready.read().bits() == 0 {};
 
 	p.SPI0.rxd.read().bits();
 }
 
 #[allow(dead_code)]
-pub fn tx_data(p: &nrf52832_pac::Peripherals, spiline: &SpiLine, data: &[u8]) {
-	set_sel(p, spiline, true);
+pub fn tx_data(p: &nrf52832_pac::Peripherals, data: &[u8]) {
+	if !unsafe { _INITIALIZED }{
+		init(p);
+	}
 
-	for i in 0..(data.len()-1) {
+	for i in 0..data.len() {
 		p.SPI0.txd.write(|w| unsafe { w.txd().bits(data[i]) });
 
-		while p.SPI0.events_ready.read().bits() != 0 {};
-
-		set_sel(p, spiline, false);
+		while p.SPI0.events_ready.read().bits() == 0 {};
 
 		p.SPI0.rxd.read().bits();
-	}
-}
-
-#[allow(dead_code)]
-fn set_sel(p: &nrf52832_pac::Peripherals, spiline: &SpiLine, is_transferring: bool) {
-	if is_transferring {
-		gpio::set_pin_state(p, spiline.sel_pin, gpio::PinState::PinLow);
-	}
-	else {
-		gpio::set_pin_state(p, spiline.sel_pin, gpio::PinState::PinHigh);
 	}
 }
 
