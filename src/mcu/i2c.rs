77eeabbce17ada11088325c628ec20efaa99b093
Taylor Spencer <tayspen72@gmail.com>
2021-02-27 21:59:34 -0700

    Moved main pins to mcu library

--- src/mcu/i2c.rs
+++ src/mcu/i2c.rs
@@ -6,6 +6,7 @@
 //==============================================================================
 // Crates and Mods
 //==============================================================================
+use crate::config;
 use nrf52832_pac;
 
 //==============================================================================
@@ -16,7 +17,6 @@ pub struct I2cLine {
 	pub scl_pin: u8,
 	pub sda_pin: u8,
 	pub frequency: nrf52832_pac::twi0::frequency::FREQUENCY_A,
-	pub address: u8,
 }
 
 //==============================================================================
@@ -27,29 +27,34 @@ pub struct I2cLine {
 //==============================================================================
 // Variables
 //==============================================================================
-
+static _I2C_LINE: I2cLine = I2cLine {
+	scl_pin: config::I2C_SCL_PIN,
+	sda_pin: config::I2C_SDA_PIN,
+	frequency: config::I2C_FREQUENCY,
+};
 
 //==============================================================================
 // Implementations
 //==============================================================================
 #[allow(dead_code)]
-pub fn init(p: &nrf52832_pac::Peripherals, i2cline: &I2cLine){
+pub fn init(p: &nrf52832_pac::Peripherals){
 	let i2c = &p.TWI0;
 	
 	i2c.enable.write(|w| w.enable().disabled());
 	
-	i2c.pselscl.write(|w| unsafe { w.bits(i2cline.scl_pin as u32) } );
-	i2c.pselsda.write(|w| unsafe { w.bits(i2cline.sda_pin as u32) } );
-	i2c.frequency.write(|w| w.frequency().variant(i2cline.frequency));
-	i2c.address.write(|w| unsafe { w.address().bits(i2cline.address) } );
+	i2c.pselscl.write(|w| unsafe { w.bits(_I2C_LINE.scl_pin as u32) } );
+	i2c.pselsda.write(|w| unsafe { w.bits(_I2C_LINE.sda_pin as u32) } );
+	i2c.frequency.write(|w| w.frequency().variant(_I2C_LINE.frequency));
 	
 	i2c.enable.write(|w| w.enable().enabled());
 }
 
 #[allow(dead_code)]
-pub fn write_byte(p: &nrf52832_pac::Peripherals, byte: u8, send_start: bool, send_stop: bool) -> Option<bool>{	
+pub fn write_byte(p: &nrf52832_pac::Peripherals, address: u8, byte: u8, send_start: bool, send_stop: bool) -> Option<bool>{	
 	let i2c = &p.TWI0;
-	
+
+	set_address(p, address);
+
 	// Preload the Txd register for sending
 	i2c.txd.write(|w| unsafe { w.txd().bits(byte) } );
 	
@@ -78,13 +83,13 @@ pub fn write_byte(p: &nrf52832_pac::Peripherals, byte: u8, send_start: bool, sen
 }
 
 #[allow(dead_code)]
-pub fn write_data(p: &nrf52832_pac::Peripherals, data: &[u8], send_start: bool, send_stop: bool) -> Option<bool> {
+pub fn write_data(p: &nrf52832_pac::Peripherals, address: u8, data: &[u8], send_start: bool, send_stop: bool) -> Option<bool> {
 	if send_start {
 		p.TWI0.tasks_starttx.write(|w| unsafe { w.bits(1) } );
 	}
 	
 	for i in 0..data.len() {
-		match write_byte(p, data[i], false, send_stop && (i == (data.len() - 1))) {
+		match write_byte(p, address, data[i], false, send_stop && (i == (data.len() - 1))) {
 			None => return None,
 			_ => ()
 		};
@@ -94,8 +99,10 @@ pub fn write_data(p: &nrf52832_pac::Peripherals, data: &[u8], send_start: bool, 
 }
 
 #[allow(dead_code)]
-pub fn read_byte(p: &nrf52832_pac::Peripherals, send_stop: bool) -> Option<u8> {
+pub fn read_byte(p: &nrf52832_pac::Peripherals, address: u8, send_stop: bool) -> Option<u8> {
 	let i2c = &p.TWI0;
+
+	set_address(p, address);
 	
 	// Start Rx task
 	i2c.tasks_startrx.write(|w| unsafe { w.bits(1) } );
@@ -120,6 +127,21 @@ pub fn read_byte(p: &nrf52832_pac::Peripherals, send_stop: bool) -> Option<u8> {
 	
 	// Pull out byte
 	Some(i2c.rxd.read().rxd().bits())
+}
+
+fn set_address(p: &nrf52832_pac::Peripherals, address: u8) {
+	static mut CURRENT_ADDRESS: u8 = 0;
+
+	if address == unsafe { CURRENT_ADDRESS } {
+		return;
+	}
+
+	if p.TWI0.address.read().bits() as u8 == address {
+		return;
+	}
+
+	p.TWI0.address.write(|w| unsafe { w.address().bits(address) } );
+	unsafe { CURRENT_ADDRESS = address };
 }
 
 //==============================================================================
