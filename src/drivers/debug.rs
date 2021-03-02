25c8186ee7211b2b3b06ba0e4b37a90db49dbd9e
Taylor Spencer <tayspen72@gmail.com>
2021-03-01 22:55:43 -0700

    Initial debug library functional

--- src/drivers/debug.rs
+++ src/drivers/debug.rs
@@ -12,7 +12,7 @@
 //==============================================================================
 // Enums, Structs, and Types
 //==============================================================================
-#[allow(dead_code)]
+use super::lcd::{lcd_api, font};
 
 //==============================================================================
 // Macros
@@ -22,42 +22,48 @@
 //==============================================================================
 // Variables
 //==============================================================================
- 
+ static mut DEBUG_CURRENT_X: u16 = 9;
+ static mut DEBUG_CURRENT_Y: u16 = 9;
+ const DEBUG_SCALE: u16 = 2;
+ const DEBUG_BACKGROUND: u16 = lcd_api::Color::Black as u16;
+ const DEBUG_FOREGROUND: u16 = lcd_api::Color::White as u16;
 
 //==============================================================================
 // Implementations
 //==============================================================================
 #[allow(dead_code)]
-pub fn init(_p: &nrf52832_pac::Peripherals) {}
-// 	let line = get_uartline();
-	
-// 	uart::init(p, &line);
+pub fn init(p: &nrf52832_pac::Peripherals) {
+	write_line(p, "**********");
+	write_line(p, "*  Debug *");
+	write_line(p, "**********");
+}
 
-// 	debug_write_string(p, "*************************");
-// 	debug_write_string(p, "*  Debugger Initialied  *");
-// 	debug_write_string(p, "*************************");
-// }
+pub fn write_line(p: &nrf52832_pac::Peripherals, string: &str) {
+	let bytes = string.as_bytes();
+	let len = bytes.len();
 
-// pub fn debug_write_string(p: &nrf52832_pac::Peripherals, string: &str) {
-// 	let bytes = string.as_bytes();
-// 	for i in 0..string.len() {
-// 		uart::tx(p, bytes[i]);
-// 	}
-// }
+	for i in 0..len {
+		write_character(p, bytes[i] as char);
+	}
 
-// fn get_uartline() -> &'static uart::UartLine {
-// 	static UARTLINE: uart::UartLine = uart::UartLine {
-// 		cts_pin: config::UART_CTS_PIN,
-// 		rts_pin: config::UART_RTS_PIN,
-// 		rx_pin: config::UART_RX_PIN,
-// 		tx_pin: config::UART_TX_PIN,
-// 		baud: config::UART_BAUD,
-// 		parity: config::UART_PARITY,
-// 		echo_enabled: config::UART_ECHO
-// 	};
-	
-// 	&UARTLINE
-// }
+	unsafe {
+		DEBUG_CURRENT_X = 9;
+		let character_height: u16 = (font::MINIMAL_CHARACTER_HEIGHT + 1) * DEBUG_SCALE;
+
+		DEBUG_CURRENT_Y += character_height;
+		if (DEBUG_CURRENT_Y + character_height) > 240 {
+			DEBUG_CURRENT_Y = 9;
+		}
+	}
+
+}
+
+fn write_character(p: &nrf52832_pac::Peripherals, c: char) {
+	unsafe {
+		font::write_minimal_character(p, c, DEBUG_CURRENT_X, DEBUG_CURRENT_Y, DEBUG_FOREGROUND, DEBUG_BACKGROUND, DEBUG_SCALE);
+		DEBUG_CURRENT_X += (font::MINIMAL_CHARACTER_WIDTH + 1) * DEBUG_SCALE;
+	}
+}
 
 //==============================================================================
 // Interrupt Handler
