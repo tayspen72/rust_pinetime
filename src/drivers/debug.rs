9ea6b2c7b672a7b6a89cd33c70795ea68ce84fcb
Taylor Spencer <tayspen72@gmail.com>
2021-03-11 10:25:31 -0700

    Debug log functional

--- src/drivers/debug.rs
+++ src/drivers/debug.rs
@@ -6,22 +6,22 @@
 // need to be built. Later.
 // The log can be hidden in real-time, as needed. Maybe with a swipe up action?
 
-// TODO: make globals into mutexes
-
 //==============================================================================
 // Crates and Mods
 //==============================================================================
+use core::cell::Cell;
+use cortex_m::interrupt::{free, Mutex};
 use super::app;
 use super::lcd::{lcd_api, font};
-use crate::mcu::rtc;
 
 //==============================================================================
 // Enums, Structs, and Types
-//==============================================================================
+//=============================================================================
+#[derive(Copy, Clone)]
 struct LogLine{
 	active: bool,
-	timestamp: u32,
-	line: &'static str
+	stale: bool,
+	line: [char; 24]
 }
 
 //==============================================================================
@@ -32,69 +32,146 @@ struct LogLine{
  const DEBUG_SCALE: u16 = 2;
  const DEBUG_BACKGROUND: u16 = lcd_api::Color::Black as u16;
  const DEBUG_FOREGROUND: u16 = lcd_api::Color::White as u16;
+ const DEBUG_WELCOME: [char; 24] = [
+	'*', '*', ' ', ' ', ' ', ' ', 'D', 'e', 'b', 'u', 'g', ' ', 
+	'O', 'u', 't', 'p', 'u', 't', ' ', ' ', ' ', ' ', '*', '*'
+ ];
 
-static mut _LOG_LINES: [LogLine; 6] = [
-	LogLine { active: true, timestamp: 0, line:  "**  Debug Output  **" },
-	LogLine { active: false, timestamp: 0, line: "--------------------" },
-	LogLine { active: false, timestamp: 0, line: "--------------------" },
-	LogLine { active: false, timestamp: 0, line: "--------------------" },
-	LogLine { active: false, timestamp: 0, line: "--------------------" },
-	LogLine { active: false, timestamp: 0, line: "--------------------" }
-];
+static LOG_LINES_ACTIVE: Mutex<Cell<usize>> = Mutex::new(Cell::new(0));
+static LOG_LINE_COUNT: Mutex<Cell<u8>> = Mutex::new(Cell::new(0));
+const LOG_LINE_ENTRIES: usize = 5;
+static LOG_LINES: Mutex<Cell<[LogLine; 6]>> = Mutex::new(Cell::new( [
+	LogLine { active: true, stale: true, line: DEBUG_WELCOME },
+	LogLine { active: false, stale: true, line: [ '-'; 24 ] },
+	LogLine { active: false, stale: true, line: [ '-'; 24 ] },
+	LogLine { active: false, stale: true, line: [ '-'; 24 ] },
+	LogLine { active: false, stale: true, line: [ '-'; 24 ] },
+	LogLine { active: false, stale: true, line: [ '-'; 24 ] }
+]));
 
 //==============================================================================
 // Public Functions
 //==============================================================================
-#[allow(dead_code)]
 pub fn init() {
-	write_line(0);
-	write_line(1);
-	write_line(2);
 
-	unsafe {
-		_LOG_LINES[0].line = "* Debug Initialized ";
-	}
 }
 
 #[allow(dead_code)]
 pub fn push_log(string: &'static str) {
-	let index = get_next_log_index();
-	unsafe { 
-		_LOG_LINES[index].active = true;
-		_LOG_LINES[index].timestamp = rtc::get_timestamp();
-		_LOG_LINES[index].line = string;
-	};
+	if free(|cs| LOG_LINES_ACTIVE.borrow(cs).get()) == LOG_LINE_ENTRIES {
+		pop_log();
+	}
+
+	free(|cs| LOG_LINES_ACTIVE.borrow(cs).set(LOG_LINES_ACTIVE.borrow(cs).get() + 1));
+	
+	let index = free(|cs| LOG_LINES_ACTIVE.borrow(cs).get());
+	let len = if string.len() > 21 { 21 } else { string.len() };
+	let string = string.as_bytes();
+	let count = free(|cs| LOG_LINE_COUNT.borrow(cs).get());
+	let count = if count + 1 == 100 { 0 } else { count + 1 };
+	free(|cs| LOG_LINE_COUNT.borrow(cs).set(count));
+
+	free(|cs| {
+		let mut log_lines = LOG_LINES.borrow(cs).get();
+		log_lines[index].active = true;
+		log_lines[index].stale = true;
+		// Print debug line count
+		log_lines[index].line[0] = (0x30 + ((count / 10) % 10)) as char;
+		log_lines[index].line[1] = (0x30 + (count % 10)) as char;
+		log_lines[index].line[2] = ':';
+
+		// Copy bytes from string into the log lines object
+		for i in 0..len {
+			log_lines[index].line[i+3] = string[i] as char;
+		}
+		if len < 21 {
+			log_lines[index].line[len+3] = 0 as char;
+		}
+
+		LOG_LINES.borrow(cs).set(log_lines);
+	});
 }
 
-//TODO: Add functions to convert numbers to strings with black magic
+pub fn push_log_number(string: &'static str, num: &u32) {
+	if free(|cs| LOG_LINES_ACTIVE.borrow(cs).get()) == LOG_LINE_ENTRIES {
+		pop_log();
+	}
+
+	free(|cs| LOG_LINES_ACTIVE.borrow(cs).set(LOG_LINES_ACTIVE.borrow(cs).get() + 1));
+	
+	let index = free(|cs| LOG_LINES_ACTIVE.borrow(cs).get());
+	let string_len = if string.len() > 21 { 21 } else { string.len() };
+	let string = string.as_bytes();
+	let count = free(|cs| LOG_LINE_COUNT.borrow(cs).get());
+	let count = if count + 1 == 100 { 0 } else { count + 1 };
+	let num_len = get_num_len(*num);
+
+	free(|cs| LOG_LINE_COUNT.borrow(cs).set(count));
+
+	free(|cs| {
+		let mut log_lines = LOG_LINES.borrow(cs).get();
+		log_lines[index].active = true;
+		log_lines[index].stale = true;
+		// Print debug line count
+		log_lines[index].line[0] = (0x30 + ((count / 10) % 10)) as char;
+		log_lines[index].line[1] = (0x30 + (count % 10)) as char;
+		log_lines[index].line[2] = ':';
+
+		// Copy bytes from string into the log lines object
+		for i in 0..string_len {
+			log_lines[index].line[i+3] = string[i] as char;
+		}
+		let mut div: u32 = 1;
+		for i in 0..num_len {
+			let c = 3 + string_len + num_len - 1 - i;
+			log_lines[index].line[c] = 
+				((0x30 + ((num / div) % 10)) as u8) as char;
+			div *= 10;
+		}
+
+		if string_len + num_len < 21 {
+			log_lines[index].line[string_len+num_len+3] = 0 as char;
+		}
+
+		LOG_LINES.borrow(cs).set(log_lines);
+	});
+}
 
 //==============================================================================
 // Private Functions
 //==============================================================================
-fn clear_line(_line_number: usize) {
-	
+fn clear_line(line_number: usize) {
+	let y = DEBUG_INITIAL_Y + ((line_number as u16) * font::MINIMAL_CHARACTER_HEIGHT * DEBUG_SCALE);
+	lcd_api::fill_rectangle(0, 240, y, font::MINIMAL_CHARACTER_HEIGHT * DEBUG_SCALE, DEBUG_BACKGROUND);
 }
 
-fn get_next_log_index() -> usize {
-	unsafe { 
-		for i in 1.._LOG_LINES.len() {
-			if !_LOG_LINES[i].active {
-				return i;
-			}
-		}
-
-		// If all cells are full, empty the oldest and shift all up
-		pop_log();
-		_LOG_LINES.len() as usize
+fn get_num_len(mut num: u32) -> usize {
+	let mut len: usize = 1;
+	num /= 10;
+	while num > 0 {
+		len += 1;
+		num /= 10;
 	}
+
+	len
 }
 
 fn pop_log() {
-	unsafe { 
-		for i in 1..(_LOG_LINES.len() - 1) {
-			_LOG_LINES[i].line = _LOG_LINES[i+1].line;
+	// Shift all entries up one - leaving the bottom entry available
+	let num_entries = free(|cs| LOG_LINES_ACTIVE.borrow(cs).get());
+	free(|cs| {
+		let mut log_lines = LOG_LINES.borrow(cs).get();
+		for i in 1..num_entries {
+			log_lines[i].active = true;
+			log_lines[i].stale = true;
+			log_lines[i].line = log_lines[i+1].line;
 		}
-	}
+		log_lines[num_entries].active = false;
+		LOG_LINES.borrow(cs).set(log_lines);
+	});
+
+	// Show that a line has just been popped
+	free(|cs| LOG_LINES_ACTIVE.borrow(cs).set(LOG_LINES_ACTIVE.borrow(cs).get() - 1));
 }
 
 fn write_character(c: char, x: u16, y: u16) {
@@ -102,18 +179,27 @@ fn write_character(c: char, x: u16, y: u16) {
 }
 
 fn write_line(line_number: usize) {
-	// TODO: use fill_rect funtion to clear this line before writing
-
-	let bytes = unsafe { _LOG_LINES[line_number].line.as_bytes() };
+	let bytes = free(|cs| LOG_LINES.borrow(cs).get()[line_number].line);
 	let len = bytes.len();
 
 	let mut x = DEBUG_INITIAL_X;
 	let y = DEBUG_INITIAL_Y + ((line_number as u16) * font::MINIMAL_CHARACTER_HEIGHT * DEBUG_SCALE);
 
 	for i in 0..len {
+		if bytes[i] == 0 as char{
+			break;
+		}
+		
 		write_character(bytes[i] as char, x, y);
 		x += font::MINIMAL_CHARACTER_WIDTH * DEBUG_SCALE;
 	}
+
+	// Update the stale line flag showing it has been displayed
+	free(|cs| {
+		let mut log_lines = LOG_LINES.borrow(cs).get();
+		log_lines[line_number].stale = false;
+		LOG_LINES.borrow(cs).set(log_lines);
+	});
 }
 
 //==============================================================================
@@ -126,14 +212,17 @@ fn write_line(line_number: usize) {
 //==============================================================================
 pub fn task_handler(d: &app::DeviceInfo) {
 	if d.flags.debug_log_active {
-		unsafe {
-			let len = _LOG_LINES.len();
-			for i in 0..len {
-				clear_line(i);
+		let len = free(|cs| LOG_LINES_ACTIVE.borrow(cs).get());
 
-				if _LOG_LINES[i].active {
-					write_line(i);
-				}
+		for i in 0..=len {
+			// If log lines are current, do nothing
+			if !free(|cs| LOG_LINES.borrow(cs).get()[i].active) {
+				return;
+			}
+
+			if free(|cs| LOG_LINES.borrow(cs).get()[i].stale) {
+				clear_line(i);
+				write_line(i);
 			}
 		}
 	}
