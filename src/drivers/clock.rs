14e685bd6471542f0d62d3f51d09a46122675656
Taylor Spencer <tayspen72@gmail.com>
2021-03-12 22:57:00 -0700

    Initial writing of clock library

--- src/drivers/clock.rs
+++ src/drivers/clock.rs
@@ -8,7 +8,7 @@
 //==============================================================================
 use core::cell::Cell;
 use cortex_m::interrupt::{free, Mutex};
-use crate::drivers::app;
+use crate::drivers::{app, lcd};
 use crate::mcu::rtc;
 
 //==============================================================================
@@ -25,17 +25,62 @@ pub struct Time {
 // Variables
 //==============================================================================
 static TIME: Mutex<Cell<Time>> = Mutex::new(Cell::new( Time {
-	hours: 0,
-	minutes: 0,
+	hours: 22,
+	minutes: 54,
 	seconds: 0
 }));
 
+const DIGITS_X: [u16; 6] = [ 0, 40, 80, 120, 160, 200 ];
+const DIGITS_Y: [u16; 6] = [ 60; 6 ];
 
 //==============================================================================
 // Public Functions
 //==============================================================================
 pub fn init() {
+	free(|cs| {
+		let mut time = TIME.borrow(cs).get();
+		time.seconds += 1;
 
+		if time.seconds >= 60 {
+			time.seconds = 0;
+			time.minutes += 1;
+		
+			if time.minutes >= 60 {
+				time.minutes = 0;
+				time.hours += 1;
+				
+				if time.hours >= 24 {
+					time.hours = 0;
+				}
+			}
+		}
+
+		TIME.borrow(cs).set(time);
+	});
+	write_time();
+}
+
+pub fn write_time() {
+	static mut LAST_DIGITS: [u8; 6] = [10; 6];	// Init at 10 to force write the first time
+	
+	let time = free(|cs| TIME.borrow(cs).get());
+	let digits: [u8; 6] = [
+		(time.hours/10)%10,
+		time.hours%10,
+		(time.minutes/10)%10,
+		time.minutes%10,
+		(time.seconds/10)%10,
+		time.seconds%10,
+	];
+
+	unsafe {
+		for i in 0..6 {
+			if digits[i] != LAST_DIGITS[i] {
+				LAST_DIGITS[i] = digits[i];
+				lcd::font::write_time_character(digits[i], DIGITS_X[i], DIGITS_Y[i], lcd::lcd_api::Color::Blue as u16, lcd::lcd_api::Color::White as u16);
+			}
+		}
+	}
 }
 
 //==============================================================================
@@ -80,6 +125,8 @@ pub fn task_handler(d: &mut app::DeviceInfo) {
 			LAST_TIMESTAMP = rtc::get_timestamp();
 			update_add_second();
 			d.time = free(|cs| TIME.borrow(cs).get());
+
+			write_time();
 		}
 	}
 
