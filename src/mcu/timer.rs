6376682948239779ee7cd1dfd8ae4b88ee8b69b8
Taylor Spencer <tayspen72@gmail.com>
2021-03-17 14:54:40 -0600

    Use DerefMut and added timer multiplier

--- src/mcu/timer.rs
+++ src/mcu/timer.rs
@@ -9,6 +9,7 @@
 // Crates and Mods
 //==============================================================================
 use core::cell::{Cell, RefCell};
+use core::ops::DerefMut;
 use cortex_m::interrupt::{free, Mutex};
 use nrf52832_pac::interrupt;
 
@@ -20,8 +21,10 @@ use nrf52832_pac::interrupt;
 //==============================================================================
 // Variables
 //==============================================================================
-static TIMER_RUNNING: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
+const TIMER_MULTIPLIER: u32 = 10;
+
 static TIMER_COUNT: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+static TIMER_RUNNING: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
 
 static TIMER_HANDLE: Mutex<RefCell<Option<nrf52832_pac::TIMER0>>> = 
 	Mutex::new(RefCell::new(None));
@@ -60,21 +63,21 @@ pub fn delay(milliseconds: u32) {
 //==============================================================================
 // Private Functions
 //==============================================================================
-fn configure (t: &nrf52832_pac::TIMER0) {
+fn configure (timer: &nrf52832_pac::TIMER0) {
 	nrf52832_pac::NVIC::mask(nrf52832_pac::Interrupt::TIMER0);
 
 	// Stop the timer before init for good measure 
-	t.tasks_stop.write(|w| unsafe{ w.bits(1) });
-	t.tasks_clear.write(|w| unsafe { w.bits(1) });
+	timer.tasks_stop.write(|w| unsafe{ w.bits(1) });
+	timer.tasks_clear.write(|w| unsafe { w.bits(1) });
 	
 	// Define timer mode and config
-	t.mode.write(|w| w.mode().timer());
-	t.bitmode.write(|w| w.bitmode()._16bit());
+	timer.mode.write(|w| w.mode().timer());
+	timer.bitmode.write(|w| w.bitmode()._16bit());
 	// Use prescaler 4: 16MHz / 2^7 = 125kHz-> will force 1MHz low freq clock for better power usage
-	t.prescaler.write(|w| unsafe { w.prescaler().bits(0x7) });
+	timer.prescaler.write(|w| unsafe { w.prescaler().bits(0x7) });
 
 	//Enable timer interrupts on compare 0 overflow
-	t.intenset.write(|w| w.compare0().set_bit());
+	timer.intenset.write(|w| w.compare0().set_bit());
 
 	unsafe {
 		nrf52832_pac::NVIC::unpend(nrf52832_pac::Interrupt::TIMER0);
@@ -86,27 +89,27 @@ fn configure (t: &nrf52832_pac::TIMER0) {
 
 fn enable(is_enabled: bool) {
 	free(|cs| {
-		let t = TIMER_HANDLE.borrow(cs).borrow();
-		let t0 = t.as_ref().unwrap();
+		if let Some(ref mut timer) = TIMER_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+			// Stop the timer before config 
+			timer.tasks_stop.write(|w| unsafe{ w.bits(1) });
+			timer.tasks_clear.write(|w| unsafe { w.bits(1) });
 
-		// Stop the timer before config 
-		t0.tasks_stop.write(|w| unsafe{ w.bits(1) });
-		t0.tasks_clear.write(|w| unsafe { w.bits(1) });
+			free(|cs| TIMER_RUNNING.borrow(cs).set(false));
 
-		free(|cs| TIMER_RUNNING.borrow(cs).set(false));
+			//configure the timer to repeat indefinitely until stopped
+			timer.shorts.write(|w| w 
+				.compare0_stop().disabled()
+				.compare0_clear().enabled()
+			);
 
-		//configure the timer to repeat indefinitely until stopped
-		t0.shorts.write(|w| w 
-			.compare0_stop().disabled()
-			.compare0_clear().enabled()
-		);
+			// Configure the timer to fire interrupt in 1ms intervals
+			const HIT_INTERVAL: u32 = 125 * TIMER_MULTIPLIER; // 125kHz refclk
+			timer.cc[0].write(|w| unsafe { w.bits(HIT_INTERVAL) });
 
-		// Configure the timer to fire interrupt in 1ms intervals
-		t0.cc[0].write(|w| unsafe { w.bits(125) });
-
-		if is_enabled {
-			t0.tasks_start.write(|w| unsafe { w.bits(1) });
-			free(|cs| TIMER_RUNNING.borrow(cs).set(true));
+			if is_enabled {
+				timer.tasks_start.write(|w| unsafe { w.bits(1) });
+				free(|cs| TIMER_RUNNING.borrow(cs).set(true));
+			}
 		}
 	});
 }
@@ -126,10 +129,23 @@ fn stop() {
 //==============================================================================
 #[interrupt]
 fn TIMER0() {
-	let t = unsafe { &nrf52832_pac::Peripherals::steal().TIMER0 };
-	if t.events_compare[0].read().bits() > 0 {
-		t.events_compare[0].write(|w| unsafe { w.bits(0) });
-		free(|cs| TIMER_COUNT.borrow(cs).set(TIMER_COUNT.borrow(cs).get() + 1));
+	let timer_hit: bool = free(|cs| {
+		if let Some(ref mut timer) = TIMER_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+			if timer.events_compare[0].read().bits() > 0 {
+				timer.events_compare[0].write(|w| unsafe { w.bits(0) });
+				true
+			}
+			else {
+				false
+			}
+		}
+		else {
+			false
+		}
+	});
+
+	if timer_hit {
+		free(|cs| TIMER_COUNT.borrow(cs).set(TIMER_COUNT.borrow(cs).get() + TIMER_MULTIPLIER));
 	}
 }
 
