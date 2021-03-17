03d0ed176583e93a24a52462c45752360cd75a9a
Taylor Spencer <tayspen72@gmail.com>
2021-03-17 14:13:37 -0600

    Updating to use DerefMut as suggested in embedded book

--- src/mcu/gpio.rs
+++ src/mcu/gpio.rs
@@ -8,6 +8,7 @@
 // Crates and Mods
 //==============================================================================
 use core::cell::RefCell;
+use core::ops::DerefMut;
 use cortex_m::interrupt::{free, Mutex};
 use nrf52832_pac;
 
@@ -54,7 +55,14 @@ pub fn init(p0: nrf52832_pac::P0) {
 
 #[allow(dead_code)]
 pub fn get_pin_state(pin: u8) -> PinState {
-	let read = free(|cs| GPIO_HANDLE.borrow(cs).borrow().as_ref().unwrap().in_.read().bits());
+	let read = free(|cs| 
+		if let Some(ref mut gpio) = GPIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+			gpio.in_.read().bits()
+		}
+		else {
+			0
+		}
+	);
 	match read & (1 << pin) {
 		0 => PinState::PinLow,
 		_ => PinState::PinHigh
@@ -64,37 +72,35 @@ pub fn get_pin_state(pin: u8) -> PinState {
 #[allow(dead_code)]
 pub fn pin_disable(pin: u8) {
 	free(|cs| {
-		let p = GPIO_HANDLE.borrow(cs).borrow();
-		let p0 = p.as_ref().unwrap();
-		// Set as input and disconnect the buffer
-		p0.pin_cnf[pin as usize].modify(|_, w| w.dir().input());
-		p0.pin_cnf[pin as usize].modify(|_, w| w.input().disconnect());
-
+		if let Some(ref mut gpio) = GPIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+			// Set as input and disconnect the buffer
+			gpio.pin_cnf[pin as usize].modify(|_, w| w.dir().input());
+			gpio.pin_cnf[pin as usize].modify(|_, w| w.input().disconnect());
+		}
 	});
 }
 
 #[allow(dead_code)]
 pub fn pin_setup(pin: u8, dir: DIR, state: PinState, pull: PULL){
 	free(|cs| {
-		let p = GPIO_HANDLE.borrow(cs).borrow();
-		let p0 = p.as_ref().unwrap();
-		
-		// Set direction
-		p0.pin_cnf[pin as usize].modify(|_, w| w.dir().variant(dir));
-		if let DIR::INPUT = dir {
-			p0.pin_cnf[pin as usize].modify(|_, w| w.input().connect());
-		}
-		else {
-			p0.pin_cnf[pin as usize].modify(|_, w| w.input().disconnect());
-		}
-		
-		// Set pin pull
-		p0.pin_cnf[pin as usize].modify(|_, w| w.pull().variant(pull));
+		if let Some(ref mut gpio) = GPIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+			// Set direction
+			gpio.pin_cnf[pin as usize].modify(|_, w| w.dir().variant(dir));
+			if let DIR::INPUT = dir {
+				gpio.pin_cnf[pin as usize].modify(|_, w| w.input().connect());
+			}
+			else {
+				gpio.pin_cnf[pin as usize].modify(|_, w| w.input().disconnect());
+			}
+			
+			// Set pin pull
+			gpio.pin_cnf[pin as usize].modify(|_, w| w.pull().variant(pull));
 
-		// Set output state
-		match state {
-			PinState::PinLow => p0.outclr.write(|w| unsafe {w.bits(1 << pin)}),
-			PinState::PinHigh => p0.outset.write(|w| unsafe {w.bits(1 << pin)})
+			// Set output state
+			match state {
+				PinState::PinLow => gpio.outclr.write(|w| unsafe {w.bits(1 << pin)}),
+				PinState::PinHigh => gpio.outset.write(|w| unsafe {w.bits(1 << pin)})
+			}
 		}
 	});
 }
@@ -103,10 +109,18 @@ pub fn pin_setup(pin: u8, dir: DIR, state: PinState, pull: PULL){
 pub fn set_pin_state(pin: u8, state: PinState){
 	match state {
 		PinState::PinLow => {
-			free(|cs| GPIO_HANDLE.borrow(cs).borrow().as_ref().unwrap().outclr.write(|w| unsafe { w.bits(1 << pin) }))
+			free(|cs| 
+				if let Some(ref mut gpio) = GPIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+					gpio.outclr.write(|w| unsafe { w.bits(1 << pin) })
+				}
+			)
 		},
 		PinState::PinHigh => {
-			free(|cs| GPIO_HANDLE.borrow(cs).borrow().as_ref().unwrap().outset.write(|w| unsafe { w.bits(1 << pin) }))
+			free(|cs| 
+				if let Some(ref mut gpio) = GPIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
+					gpio.outset.write(|w| unsafe { w.bits(1 << pin) })
+				}
+			)
 		}
 	}
 }
