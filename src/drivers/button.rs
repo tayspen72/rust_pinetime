bcd7400021158847e468f6fe42de56e8d0d600f2
Taylor Spencer <tayspen72@gmail.com>
2021-03-24 08:50:28 -0600

    Update button lib for input event

--- src/drivers/button.rs
+++ src/drivers/button.rs
@@ -8,9 +8,10 @@
 // Crates and Mods
 //==============================================================================
 use crate::config;
-use crate::mcu::gpio;
+use crate::mcu::{gpio, input};
 use nrf52832_pac::p0::pin_cnf::DIR_A as DIR;
 use nrf52832_pac::p0::pin_cnf::PULL_A as PULL;
+use super::app;
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -20,20 +21,31 @@ use nrf52832_pac::p0::pin_cnf::PULL_A as PULL;
 //==============================================================================
 // Variables
 //==============================================================================
+const BUTTON_PRESS_CONFIG: input::PinConfig = input::PinConfig {
+	pin: config::PUSH_BUTTON_IN_PIN,
+	polarity: nrf52832_pac::gpiote::config::POLARITY_A::HITOLO,
+	pull: nrf52832_pac::p0::pin_cnf::PULL_A::PULLUP,
+	callback: &press_handler,
+	real_time_callback: false
+};
 
+static mut UNHANDLED_PRESSES: u8 = 0;
 
 //==============================================================================
 // Public Functions
 //==============================================================================
 pub fn init() {
-	gpio::pin_setup(config::PUSH_BUTTON_IN_PIN, DIR::INPUT, gpio::PinState::PinLow, PULL::PULLUP);
+	input::init_pin(BUTTON_PRESS_CONFIG);
 	gpio::pin_setup(config::PUSH_BUTTON_OUT_PIN, DIR::OUTPUT, gpio::PinState::PinLow, PULL::DISABLED);
 }
 
 //==============================================================================
 // Private Functions
 //==============================================================================
-
+#[allow(dead_code)]
+fn press_handler() {
+	unsafe { UNHANDLED_PRESSES = UNHANDLED_PRESSES + 1 };
+}
 
 //==============================================================================
 // Interrupt Handler
@@ -43,3 +55,12 @@ pub fn init() {
 //==============================================================================
 // Task Handler
 //==============================================================================
+pub fn task_handler(d: &mut app::DeviceInfo) {
+	unsafe {
+		if UNHANDLED_PRESSES > 0 {
+			d.flags.button_press = true;
+			d.button_press_count = UNHANDLED_PRESSES;
+			UNHANDLED_PRESSES = 0;
+		}
+	}
+}
