b11b0dffbbc6c4da4184e60879dda13fa81b65f9
Taylor Spencer <tayspen72@gmail.com>
2021-03-26 22:18:33 -0600

    Record button press state into queue and handle one-by-one

--- src/drivers/button.rs
+++ src/drivers/button.rs
@@ -11,7 +11,7 @@ use crate::config;
 use crate::mcu::{gpio, input};
 use nrf52832_pac::p0::pin_cnf::DIR_A as DIR;
 use nrf52832_pac::p0::pin_cnf::PULL_A as PULL;
-use super::app;
+use super::{app, debug};
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -23,13 +23,18 @@ use super::app;
 //==============================================================================
 const BUTTON_PRESS_CONFIG: input::PinConfig = input::PinConfig {
 	pin: config::PUSH_BUTTON_IN_PIN,
-	polarity: nrf52832_pac::gpiote::config::POLARITY_A::HITOLO,
+	polarity: nrf52832_pac::gpiote::config::POLARITY_A::TOGGLE,
 	pull: nrf52832_pac::p0::pin_cnf::PULL_A::PULLUP,
 	callback: &press_handler,
 	real_time_callback: false
 };
 
+const PRESS_QUEUE_LENGTH: usize = 8;
+
 static mut UNHANDLED_PRESSES: u8 = 0;
+static mut PRESS_QUEUE: [bool; PRESS_QUEUE_LENGTH] = [false; PRESS_QUEUE_LENGTH];
+static mut HEAD: usize = 0;
+static mut TAIL: usize = 0;
 
 //==============================================================================
 // Public Functions
@@ -44,7 +49,14 @@ pub fn init() {
 //==============================================================================
 #[allow(dead_code)]
 fn press_handler() {
-	unsafe { UNHANDLED_PRESSES = UNHANDLED_PRESSES + 1 };
+	unsafe { 
+		UNHANDLED_PRESSES = UNHANDLED_PRESSES + 1;
+		PRESS_QUEUE[TAIL] = match gpio::get_pin_state(config::PUSH_BUTTON_IN_PIN) {
+			gpio::PinState::PinHigh => false,
+			gpio::PinState::PinLow => true,
+		};
+		TAIL = if (TAIL + 1) < PRESS_QUEUE_LENGTH { TAIL + 1 } else { 0 };
+	};
 }
 
 //==============================================================================
@@ -58,9 +70,19 @@ fn press_handler() {
 pub fn task_handler(d: &mut app::DeviceInfo) {
 	unsafe {
 		if UNHANDLED_PRESSES > 0 {
-			d.flags.button_press = true;
-			d.button_press_count = UNHANDLED_PRESSES;
-			UNHANDLED_PRESSES = 0;
+			d.change_flags.button_press = true;
+			d.flags.button_pressed = PRESS_QUEUE[HEAD];
+			HEAD = if (HEAD + 1) < PRESS_QUEUE_LENGTH { HEAD + 1 } else { 0 };
+			UNHANDLED_PRESSES = UNHANDLED_PRESSES - 1;
+
+			if debug::is_enabled() {
+				if d.flags.button_pressed {
+					debug::push_log("Button pressed!");
+				}
+				else {
+					debug::push_log("Button released!");
+				}
+			}
 		}
 	}
 }
