1c91e1d480f42f6abb5fb86a3f64dabd19cf6636
Taylor Spencer <tayspen72@gmail.com>
2021-03-29 09:03:28 -0600

    Printing out battery icon with voltage readout

--- src/app/display.rs
+++ src/app/display.rs
@@ -7,7 +7,8 @@
 //==============================================================================
 // Crates and Mods
 //==============================================================================
-use super::info;
+use super::{app, icon, info};
+use crate::drivers::lcd::{font, lcd_api};
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -32,7 +33,45 @@ pub enum DisplayState {
 //==============================================================================
 // Private Functions
 //==============================================================================
+pub fn write_battery_level(level: u8, voltage: u16, charging: bool) {
+	// Draw outline
+	lcd_api::fill_rectangle(icon::BATTERY_ICON_X + 7, 29, icon::BATTERY_ICON_Y, 15, icon::BATTERY_OUTLINE_COLOR);
 
+	// Fill in the middle of the battery
+	let color = if charging { icon::CHARGER_COLOR } else { icon::BATTERY_FILL_COLOR };
+	lcd_api::fill_rectangle(icon::BATTERY_ICON_X + 9, 25, icon::BATTERY_ICON_Y + 2, 11, color);
+
+	// Print out battery level blocks
+	for l in 0..level {
+		let x = icon::BATTERY_ICON_X + 10 + (l as u16 * 6);
+		lcd_api::fill_rectangle(x, 5, icon::BATTERY_ICON_Y + 3, 9, icon::BATTERY_OUTLINE_COLOR);
+	}
+
+	// Write out the voltage value
+	let mut divider: u16 = 1000;
+	let color = if charging { icon::CHARGER_COLOR } else { icon::BATTERY_OUTLINE_COLOR };
+	for d in 0..4 {
+		let v: u8 = if d == 1 {
+			0x2E
+		}
+		else {
+			divider = divider / 10;
+			((voltage / divider) % 10) as u8
+		};
+		let x = icon::BATTERY_ICON_X + 8 + (d as u16 * 6);
+		
+		font::write_minimal_character((0x30 + v) as char, x, icon::BATTERY_ICON_Y + 16, color, icon::BATTERY_FILL_COLOR, 1)
+	}
+
+	// Print (or remove) the charger status icon
+	let color = if charging { icon::CHARGER_COLOR } else { icon::BATTERY_FILL_COLOR };
+	lcd_api::fill_rectangle(icon::BATTERY_ICON_X, 7, icon::BATTERY_ICON_Y, 15, color);
+	if charging {
+		lcd_api::fill_rectangle(icon::BATTERY_ICON_X, 4, icon::BATTERY_ICON_Y, 6, icon::BATTERY_FILL_COLOR);
+		lcd_api::fill_rectangle(icon::BATTERY_ICON_X + 2, 2, icon::BATTERY_ICON_Y + 9, 6, icon::BATTERY_FILL_COLOR);
+	}
+	lcd_api::fill_rectangle(icon::BATTERY_ICON_X + 5, 2, icon::BATTERY_ICON_Y + 3, 9, icon::BATTERY_OUTLINE_COLOR);
+}
 
 //==============================================================================
 // Interrupt Handler
@@ -42,7 +81,22 @@ pub enum DisplayState {
 //==============================================================================
 // Task Handler
 //==============================================================================
-pub fn task_handler(d: &info::DeviceInfo) {
+pub fn task_handler(d: &mut info::DeviceInfo) {
+	// If any pending flags, clear them now
+	if d.change_flags.display_state {
+		d.change_flags.display_state = false;
+	}
+
+	// Update display for all newly changes flags
+	if d.change_flags.battery_voltage || d.change_flags.charger_state {
+		// Only update battery voltage when display is on and on home page
+		if let DisplayState::On = d.display_state {
+			if let app::AppPage::Home = d.app_page {
+				write_battery_level(d.battery_level as u8, d.battery_voltage, d.flags.charger_connected);
+			}
+		}
+	}
+
 	match d.display_state {
 		DisplayState::On => (),
 		DisplayState::Dim => (),
