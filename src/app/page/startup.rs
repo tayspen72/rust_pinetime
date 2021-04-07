2c878433a0659df3b128f75b7236c264b9fd0f12
Taylor Spencer <tayspen72@gmail.com>
2021-04-07 12:59:49 -0600

    Removed color test from startup

--- src/app/page/startup.rs
+++ src/app/page/startup.rs
@@ -6,7 +6,7 @@
 //==============================================================================
 // Crates and Mods
 //==============================================================================
-use crate::drivers::lcd::lcd_api;
+use crate::drivers::lcd::{font, lcd_api};
 use crate::mcu::timer;
 
 //==============================================================================
@@ -27,22 +27,25 @@ pub fn print_page(){
 	// Show the startup sequence
 	lcd_api::fill_background(lcd_api::Color::White);
 	lcd_api::set_backlight(lcd_api::BacklightBrightness::Brightness4);
+	font::write_minimal_line(b"Tsp Designs", 174, 231, lcd_api::Color::Navy, lcd_api::Color::White, 1);
 	lcd_api::write_splash();
+	font::write_minimal_line(b"Powered by", 60, 179, lcd_api::Color::Rust, lcd_api::Color::White, 2);
+	font::write_minimal_line(b"RUST", 85, 200, lcd_api::Color::Rust, lcd_api::Color::White, 3);
 	timer::delay(2000);
 
-	lcd_api::set_backlight(lcd_api::BacklightBrightness::Brightness0);
-	lcd_api::fill_background(lcd_api::Color::Black);
+	// lcd_api::set_backlight(lcd_api::BacklightBrightness::Brightness0);
+	// lcd_api::fill_background(lcd_api::Color::Black);
 
-	lcd_api::set_backlight(lcd_api::BacklightBrightness::Brightness4);
-	lcd_api::fill_rectangle(0, 79, 0, 79, lcd_api::Color::Black);
-	lcd_api::fill_rectangle(81, 78, 0, 79, lcd_api::Color::Red);
-	lcd_api::fill_rectangle(161, 79, 0, 79, lcd_api::Color::Orange);
-	lcd_api::fill_rectangle(0, 79, 81, 78, lcd_api::Color::Yellow);
-	lcd_api::fill_rectangle(81, 78, 81, 78, lcd_api::Color::Green);
-	lcd_api::fill_rectangle(161, 79, 81, 78, lcd_api::Color::Cyan);
-	lcd_api::fill_rectangle(0, 79, 161, 79, lcd_api::Color::Blue);
-	lcd_api::fill_rectangle(81, 78, 161, 79, lcd_api::Color::Magenta);
-	lcd_api::fill_rectangle(161, 79, 161, 79, lcd_api::Color::White);
+	// lcd_api::set_backlight(lcd_api::BacklightBrightness::Brightness4);
+	// lcd_api::fill_rectangle(0, 79, 0, 79, lcd_api::Color::Black);
+	// lcd_api::fill_rectangle(81, 78, 0, 79, lcd_api::Color::Red);
+	// lcd_api::fill_rectangle(161, 79, 0, 79, lcd_api::Color::Orange);
+	// lcd_api::fill_rectangle(0, 79, 81, 78, lcd_api::Color::Yellow);
+	// lcd_api::fill_rectangle(81, 78, 81, 78, lcd_api::Color::Green);
+	// lcd_api::fill_rectangle(161, 79, 81, 78, lcd_api::Color::Cyan);
+	// lcd_api::fill_rectangle(0, 79, 161, 79, lcd_api::Color::Blue);
+	// lcd_api::fill_rectangle(81, 78, 161, 79, lcd_api::Color::Magenta);
+	// lcd_api::fill_rectangle(161, 79, 161, 79, lcd_api::Color::White);
 	timer::delay(2000);
 }
 
