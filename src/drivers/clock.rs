91aeb59475f6366030591e26337cb61a09e905cd
Taylor Spencer <tayspen72@gmail.com>
2021-04-07 13:00:04 -0600

    Added analog locators for main watch appearance

--- src/drivers/clock.rs
+++ src/drivers/clock.rs
@@ -32,9 +32,29 @@ static TIME: Mutex<Cell<Time>> = Mutex::new(Cell::new( Time {
 	seconds: 0
 }));
 
-const DIGITS_X: [u16; 4] = [ 69, 109, 159, 199];
-const DIGITS_Y: [u16; 4] = [ 0; 4 ];
+const BLOCK_LARGE_COLOR: lcd::lcd_api::Color = lcd::lcd_api::Color::Gray;
+const BLOCK_SMALL_COLOR: lcd::lcd_api::Color = lcd::lcd_api::Color::GrayDark;
+const DIGITS_X: [u16; 4] = [ 35, 75, 125, 165];
+const DIGITS_Y: [u16; 4] = [ 120; 4 ];
 
+const BLOCKS: [(u16, u16, u16, lcd::lcd_api::Color); 12] = [
+	(45, 0, 4, BLOCK_LARGE_COLOR),
+	(115, 0, 8, BLOCK_SMALL_COLOR),
+	(189, 0, 4, BLOCK_SMALL_COLOR),
+	
+	(0, 45, 4, BLOCK_SMALL_COLOR),
+	(235, 45, 4, BLOCK_LARGE_COLOR),
+	
+	(0, 115, 8, BLOCK_SMALL_COLOR),
+	(231, 115, 8, BLOCK_SMALL_COLOR),
+	
+	(0, 189, 4, BLOCK_LARGE_COLOR),
+	(235, 189, 4, BLOCK_SMALL_COLOR),
+	
+	(45, 235, 4, BLOCK_SMALL_COLOR),
+	(115, 231, 8, BLOCK_SMALL_COLOR),
+	(189, 235, 4, BLOCK_LARGE_COLOR),
+];
 
 //==============================================================================
 // Public Functions
@@ -66,6 +86,13 @@ pub fn update_time(is_military_time: bool) {
 	write(get_digits(is_military_time), false);
 }
 
+pub fn write_analog() {
+	for block in BLOCKS.iter() {
+		let (x, y, size, color) = block;
+		lcd::lcd_api::fill_rectangle(*x, *size, *y, *size, *color);
+	}
+}
+
 pub fn write_time(is_military_time: bool) {
 	write(get_digits(is_military_time), true);
 }
@@ -77,7 +104,7 @@ fn get_digits(is_military_time: bool) -> [u8; 4] {
 	let time = free(|cs| TIME.borrow(cs).get());
 	let mut hours = time.hours;
 
-	// Bring in militart time flag as needed
+	// Bring in military time flag as needed
 	if !is_military_time {
 		hours = hours % 12;
 		if hours == 0 {
@@ -118,13 +145,21 @@ fn update_add_second() {
 }
 
 fn write(digits: [u8; 4], force_update: bool) {
+	if force_update {
+		write_analog();
+	}
+
 	unsafe {
 		for i in 0..4 {
 			if force_update || digits[i] != DIGITS_ON_DISPLAY[i] {
-				DIGITS_ON_DISPLAY[i] = digits[i];
-				if i > 0 || digits[i] > 0 {
-					lcd::font::write_time_character(digits[i], DIGITS_X[i], DIGITS_Y[i], lcd::lcd_api::Color::Blue, lcd::lcd_api::Color::Black);
+				if i == 0 && digits[i] == 0 {
+					lcd::lcd_api::fill_rectangle(DIGITS_X[i], lcd::font::TIME_CHARACTER_WIDTH, DIGITS_Y[i], lcd::font::TIME_CHARACTER_HEIGHT, lcd::lcd_api::Color::Black);
 				}
+				else {
+					lcd::font::write_time_character(digits[i], DIGITS_X[i], DIGITS_Y[i], lcd::lcd_api::Color::Gray, lcd::lcd_api::Color::Black);
+				}
+
+				DIGITS_ON_DISPLAY[i] = digits[i];
 			}
 		}
 	}
